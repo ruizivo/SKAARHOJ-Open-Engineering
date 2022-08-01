ea7148fe7c0a0ad537f0f8daa9223edf36a69ffc
Rui Menezes <ruizivo.menezes@gmail.com>
2022-08-01 10:31:36 -0300

    update ATEMBase to work with esp32

--- ArduinoLibs/ATEMbase/ATEMbase.h
+++ ArduinoLibs/ATEMbase/ATEMbase.h
@@ -54,7 +54,7 @@ you can keep a clear conscience: http://skaarhoj.com/about/licenses/
 class ATEMbase
 {
   protected:
-  	#ifdef ESP8266
+  	#if defined(ESP8266) || defined(ESP32)
   	WiFiUDP _Udp;
   	#else
 	EthernetUDP _Udp;					// UDP object for communication, see constructor.
